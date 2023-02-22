74453450e810bb1b3e1cc666f6a18b0830e3da06
Simon Petrac <spetrac@nicos-ag.com>
2023-02-22 14:32:20 +0100

    wip

--- src/json-turtle.js
+++ src/json-turtle.js
@@ -1,3 +1,13 @@
 const
     // https://rdf.js.org/N3.js/
     N3 = require('n3');
+
+exports.parseTurtle = function (turtle) {
+    const parser = new N3.Parser({
+        format: 'text/turtle',
+        baseIRI: 'http://localhost/',
+        blankNodePrefix: ''
+    });
+    const quads = parser.parse(turtle);
+    return new N3.Store(quads);
+};
--- test/develop.test.js
+++ test/develop.test.js
@@ -2,4 +2,32 @@ const
     // https://mochajs.org/#interfaces
     { describe, test } = require('mocha'),
     // https://www.chaijs.com/api/bdd/
-    { expect } = require('chai');
+    { expect } = require('chai'),
+    N3 = require('n3'),
+    jsonTurtle = require('../src/json-turtle.js');
+
+describe('develop', function () {
+
+    test('parseTurtle', function () {
+        const store = jsonTurtle.parseTurtle(`
+            @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+            @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+            @prefix ex: <http://example.org/> .
+
+            ex:hello 
+                a rdfs:Resource ;
+                rdfs:label "Hello World!"@en ;
+            .
+        `);
+
+        expect(store)
+            .to.be.an.instanceof(N3.Store)
+            .that.has.a.property('size')
+            .which.equals(2);
+        expect(store.getQuads())
+            .to.be.an('array').of.length(2)
+            .that.has.a.nested.property('[0].subject.value')
+            .which.equals('http://example.org/hello');
+    });
+
+});
